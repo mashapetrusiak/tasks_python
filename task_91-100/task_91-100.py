13640604d54fa0352575ee25bbc3844e09f395f6
Masha_Pertusiak <57170071+mashapetrusiak@users.noreply.github.com>
2019-11-29 16:33:22 +0200

    Update task_91-100.py

--- task_91-100/task_91-100.py
+++ task_91-100/task_91-100.py
@@ -81,15 +81,7 @@ def number_99():
 #number_99()
 
 def number_100():
-    def solve(numheads, numlegs):
-        ns = 'No solutions!'
-        for i in range(numheads + 1):
-            j = numheads - i
-            if 2 * i + 4 * j == numlegs:
-                return i, j
-        return ns, ns
-    numheads = 35
-    numlegs = 94
-    solutions = solve(numheads, numlegs)
-    print(solutions)
+    rabbits = 94 // 2 - 35
+    chickens = 35 - rabbits
+    print(rabbits, chickens)
 #number_100()
