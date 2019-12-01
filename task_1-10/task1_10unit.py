762e31cd848a28e5871d9f263e8bdc0108fe9aba
katherine <katya.pereverzeva2109@gmail.com>
2019-12-02 01:02:28 +0200

    write tests to first patch of methods

--- task_1-10/task1_10unit.py
+++ task_1-10/task1_10unit.py
@@ -1,7 +1,16 @@
 import unittest
+from unittest import mock
+from unittest.mock import patch
+
 from task_1_10 import *
 
+
 class Task10(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
 
     def test_one_valid_1(self):
         expected_result = 2002
@@ -66,6 +75,118 @@ class Task10(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_four_check_tuple_representation(self):
-        expected = print(['1']),print(('1',))
+        expected = print(['1']), print(('1',))
         actual = number_4("1")
         self.assertEqual(expected, actual)
+
+    # def test_five(self):
+    #     instance = Mystring().getString()
+
+    def test_six_on_minus(self):
+        expected = "Value should be positive"
+        actual = number_6("-1")
+        self.assertEqual(expected, actual)
+
+    def test_six_one_positive_number(self):
+        expected = "5"
+        actual = number_6("8")
+        self.assertEqual(actual, expected)
+
+    def test_six_more_positive_numbers(self):
+        expected = "18,22,24"
+        actual = number_6("100,150,180")
+        self.assertEqual(expected, actual)
+
+    def test_six_positive_and_negative(self):
+        expected = "All values should be positive"
+        actual = number_6("100,150,180,-3")
+        self.assertEqual(expected, actual)
+
+    def test_seven_one_null(self):
+        expected = "You should enter two values"
+        actual = number_7("0")
+        self.assertEqual(expected, actual)
+
+    def test_seven_two_nulls(self):
+        expected = []
+        actual = number_7("0,0")
+        self.assertEqual(expected, actual)
+
+    def test_seven_minus_values(self):
+        expected = "You'd enter positive numbers"
+        actual = number_7("-1,-2")
+        self.assertEqual(expected, actual)
+
+    def test_seven_correct_values(self):
+        expected = [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
+        actual = number_7("3,5")
+        self.assertEqual(expected, actual)
+
+    def test_eight_empty_string(self):
+        expected = "Please, input full string"
+        actual = number_8("")
+        self.assertEqual(expected, actual)
+
+    def test_eight_one_word(self):
+        expected = "ok"
+        actual = number_8("ok")
+        self.assertEqual(expected, actual)
+
+    def test_eight_non_latin(self):
+        expected = "ананас,банан"
+        actual = number_8("банан,ананас")
+        self.assertEqual(expected, actual)
+
+    def test_eight_latin(self):
+        expected = "bag,hello,without,world"
+        actual = number_8("without,hello,bag,world")
+        self.assertEqual(expected, actual)
+
+    def test_eight_if_first_letter_is_equal(self):
+        expected = "fa,fu"
+        actual = number_8("fu,fa")
+        self.assertEqual(expected, actual)
+
+    def test_nine_empty(self):
+        expected = "Please, input full string"
+        actual = number_9("")
+        self.assertEqual(expected, actual)
+
+    def test_nine_non_latin(self):
+        expected = "АНАНАС,БАНАН"
+        actual = number_9("ананас,банан")
+        self.assertEqual(expected, actual)
+
+    def test_nine_latin(self):
+        expected = "HEY"
+        actual = number_9("hey")
+        self.assertEqual(expected, actual)
+
+    def test_ten_empty(self):
+        expected = "Please, input full string"
+        actual = number_10("")
+        self.assertEqual(expected, actual)
+
+    def test_ten_check_duplicates(self):
+        string = "hello hello again again"
+        self.assertEqual(number_10(string).count("hello") + number_10(string).count("again"), 2)
+
+    def test_ten_check_sorting(self):
+        string = "hello hello again again"
+        actual = number_10(string)
+        expected = "again hello"
+        self.assertEqual(expected, actual)
+
+    def test_ten_if_first_letter_is_equal(self):
+        expected = "fa fu"
+        actual = number_10("fu fa")
+        self.assertEqual(expected, actual)
+
+    def test_ten_non_latin(self):
+        expected = "ананас банан"
+        actual = number_10("банан ананас банан")
+        self.assertEqual(expected, actual)
+
+
+if __name__ == 'main':
+    unittest.main()
