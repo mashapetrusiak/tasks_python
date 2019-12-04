b10f4118ed9913fd3a2d3cc81249ca034ae98140
katherine <katya.pereverzeva2109@gmail.com>
2019-12-04 14:40:42 +0200

    simplify tasks outputs

--- task_31-40/task_31_40.py
+++ task_31-40/task_31_40.py
@@ -1,23 +1,23 @@
-def number_31(word1, word2):
+def number_31(inp):
+    word1, word2 = inp.split()
     len1 = len(word1)
     len2 = len(word2)
     if len1 > len2:
-        print(word1)
+        return word1
     elif len1 < len2:
-        print(word2)
+        return word2
     else:
-        print(word1)
-        print(word2)
+        return word1 + '\n' + word2
 
 
-# word1,word2=input().split()
-# number_31(word1,word2)
+# print(number_31(input()))
+
 
 def number_32(a):
-    if a % 2 == 0:
-        print("It is an even number")
+    if int(a) % 2 == 0:
+        return "It is an even number"
     else:
-        print("It is an odd number")
+        return "It is an odd number"
 
 
 # a = int(input("Number: "))
@@ -27,73 +27,70 @@ def number_33():
     d = dict()
     for i in range(1, 4):
         d[i] = i ** 2
-    print(d)
+    return d
 
 
-# number_33()
+# print(number_33())
 
 def number_34():
     d = dict()
     for i in range(1, 21):
         d[i] = i ** 2
-    print(d)
+    return d
 
 
-# number_34()
+# print(number_34())
 
 def number_35():
     d = dict()
     for i in range(1, 21):
         d[i] = i ** 2
-    for x in d.values():
-        print(x)
+    return d.values()
 
 
-# number_35()
+# print(number_35())
 
 def number_36():
     d = dict()
     for i in range(1, 21):
         d[i] = i ** 2
-    for x in d.keys():
-        print(x)
+    return d.keys()
 
 
-# number_36()
+# print(number_36())
 
 def number_37():
     lst = list()
     for i in range(1, 21):
         lst.append(i ** 2)
-    print(lst)
+    return lst
 
 
-# number_37()
+# print(number_37())
 
 def number_38():
     lst = list()
     for i in range(1, 21):
         lst.append(i ** 2)
-    for i in range(5):
-        print(lst[i])
+    return lst[:5]
 
 
-# number_38()
+# print(number_38())
 
 def number_39():
     lst = list()
     for i in range(1, 21):
         lst.append(i ** 2)
-    for i in range(19, 14, -1):
-        print(lst[i])
+    return lst[-5:]
 
 
-# number_39()
+# print(number_39())
 
 def number_40():
     lst = list()
     for i in range(1, 21):
         lst.append(i ** 2)
-    for i in range(5, 20):
-        print(lst[i])
-# number_40()
+    return lst[5:]
+
+
+print(number_40())
